2035eedd1d8a50aab3a902764bd2d272c471dbfa
chantie84 <cc.crompvoets@gmail.com>
2025-04-08 13:12:27 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      {" "}
-      <h1>Weather App</h1>
-      <footer>
-        This project is created by Chantal Crompvoets and is {""}
-        <a
-          href="https://github.com/chantie84/my-react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        {" "}
+        <h1>Weather App</h1>
+        <footer>
+          This project is created by Chantal Crompvoets and is {""}
+          <a
+            href="https://github.com/chantie84/my-react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

aacb5517990bcf0d98c119a32d6717ad81d61014
chantie84 <cc.crompvoets@gmail.com>
2025-04-13 16:05:40 +0200

    Added weather icons

--- src/Weather.js
+++ src/Weather.js
@@ -13,7 +13,7 @@ export default function Weather(props) {
       humidity: response.data.temperature.humidity,
       date: new Date(response.data.time * 1000),
       description: response.data.condition.description,
-      iconUrl: `https://openweathermap.org/img/wn/${response.data.condition.icon}@2x.png`,
+      icon: response.data.condition.icon,
       wind: response.data.wind.speed,
       city: response.data.city,
     });
--- src/Weatherinfo.js
+++ src/Weatherinfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function Weatherinfo(props) {
   return (
@@ -15,11 +16,9 @@ export default function Weatherinfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
-            <img
-              src={props.data.iconUrl}
-              alt={props.data.description}
-              className="float-start"
-            />
+            <div className="float-start">
+              <WeatherIcon code={props.data.icon} />
+            </div>
             <div className="float-start">
               <span className="temperature">
                 {Math.round(props.data.temperature)}

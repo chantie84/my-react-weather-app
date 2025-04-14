aaeb704654b9063ccec5093689ccc5bfa02fdf1e
chantie84 <cc.crompvoets@gmail.com>
2025-04-14 22:41:41 +0200

    Added img background and made some style changes

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -25,7 +25,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="#212121"
+      color="white"
       size={52}
       animate={true}
     />
